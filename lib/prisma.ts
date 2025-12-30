0029e6b759fabe000177794afb7ff0b114aed933
Nachooop17 <mic.ibanez@duocuc.cl>
2025-12-29 22:51:44 -0300

    0.3

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,24 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+// Definimos la función que crea la conexión
+const prismaClientSingleton = () => {
+  // Si estamos en Vercel (Producción), usamos el adaptador de Turso
+  if (process.env.NODE_ENV === 'production') {
+    const adapter = new PrismaLibSql({
+      url: process.env.DATABASE_URL!,
+      authToken: process.env.TURSO_AUTH_TOKEN
+    })
+    return new PrismaClient({ adapter })
+  }
+  
+  // Si estamos en tu PC, usamos la base de datos normal
+  return new PrismaClient()
+}
+
+export const prisma = globalForPrisma.prisma || prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
